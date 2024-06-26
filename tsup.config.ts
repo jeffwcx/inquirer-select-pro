0d31cb227fa81abee081bbbcb82b22ccb51f5fd2
jeffwcx <jeffwcx@icloud.com>
2024-06-27 00:59:52 +0800

    fix: cjs require esm error
    solution: bundle all deps when outputting in cjs format
    
    #1

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig([
   {
-    name: 'select-pro',
+    name: 'select-pro-esm',
     entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
+    format: ['esm'],
     platform: 'node',
+    dts: true,
+    sourcemap: true,
+  },
+  {
+    name: 'select-pro-cjs',
+    entry: ['src/index.ts'],
+    format: ['cjs'],
+    platform: 'node',
+    noExternal: [/(.*)/],
     dts: true,
     sourcemap: true,
   },
